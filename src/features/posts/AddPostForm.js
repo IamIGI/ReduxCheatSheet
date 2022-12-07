9113c893c6638eafb8a6f8b95ae937f00ca2e71b
i.kłusek <J6ZkqwVUn>
2022-12-07 14:49:50 +0100

    Async thunk middleware with redux toolkit

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { postAdded } from './postsSlice';
+import { addNewPost } from './postsSlice';
 import { selectAllUsers } from '../users/usersSlice';
 
 const AddPostForm = () => {
@@ -10,19 +10,31 @@ const AddPostForm = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [userId, setUserId] = useState('');
+    const [addRequestStatus, setAddRequestStatus] = useState('idle');
 
     const users = useSelector(selectAllUsers);
 
     const onTitleChanged = (e) => setTitle(e.target.value);
     const onContentChanged = (e) => setContent(e.target.value);
     const onAuthorChanged = (e) => setUserId(e.target.value);
+    //every check is every item pass the test - in this example is Boolean,
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
 
     const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(postAdded(title, content, userId));
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending');
+                //addNewPost can throw and error
+                dispatch(addNewPost({ title, body: content, userId })).unwrap();
 
-            setTitle('');
-            setContent('');
+                setTitle('');
+                setContent('');
+                setUserId('');
+            } catch (err) {
+                console.error('Failed to save the post', err);
+            } finally {
+                setAddRequestStatus('idle');
+            }
         }
     };
 
@@ -31,8 +43,6 @@ const AddPostForm = () => {
             {user.name}
         </option>
     ));
-
-    const canSave = Boolean(title) && Boolean(userId) && Boolean(content);
 
     return (
         <section>
